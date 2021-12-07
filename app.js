ae0728ddf100fc5ccb2a9585a3375bb9d678a794
cai1994214 <1027573911@qq.com>
2021-12-07 23:14:04 +0800

    add nodemon

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-function add() {
+"use strict";
+exports.__esModule = true;
+function add1() {
     console.log('hello world');
 }
+console.log('123');
+add1();
--- app.ts
+++ app.ts
@@ -1,3 +1,8 @@
 function add1 (): void {
     console.log('hello world')
 }
+
+console.log('123')
+
+add1()
+export {}

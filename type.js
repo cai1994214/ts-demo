117ea150ac0d9791fdaae8d3cb6d17a611cfb803
cai1994214 <1027573911@qq.com>
2021-12-13 22:36:01 +0800

    ts type

--- type.js
+++ type.js
@@ -1,33 +1,9 @@
-//number;
-var n;
-n = 10;
-console.log(n);
-//string;
-var s;
-s = '1';
-console.log(s);
-//boolean;
-var b;
-b = true;
-console.log(b);
-//字面量 联合数据类型 string | number | boolean
-var text;
-text = 10;
-text = '联合数据';
-console.log(text);
-//any
-var a;
-a = 1;
-a = 's';
-a = true;
-console.log(a);
-//未知类型
-var u;
-var s1;
-// s1 = a; //any可以赋值 不報錯
-// s1 = u;//unknown 类型的不能赋值 
-//类型断言 可以用来告诉解析器变量的实际类型
-s1 = u;
-s1 = u;
-function fn() {
-}
+//基本类型
+var m;
+var k;
+var l;
+var k1;
+var l1;
+k1 = 2;
+k1 = 3;
+console.log(k1);

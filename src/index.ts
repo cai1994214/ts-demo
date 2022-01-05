6995455175be93e1785e3ef90e1c4df19c4d1fd8
cai1994214 <1027573911@qq.com>
2022-01-05 23:41:39 +0800

    add food class

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,30 @@
-import './style/index.less'
-console.log(123)
+import './style/index.less';
+
+class Food {
+    element: HTMLElement;
+
+    constructor () {
+        this.element = document.getElementById("fond") as HTMLElement;
+    }
+
+    //获取当前坐标
+    get X() {
+        return this.element.offsetLeft;
+    }
+
+    get Y() {
+        return this.element.offsetTop;
+    }
+
+    change() {
+        //出现位置 随机 最小0 最大290 并且是整10数
+        let top = Math.round(Math.random() * 29) * 10;
+        let left = Math.round(Math.random() * 29) * 10;
+        this.element.style.left = left + 'px';
+        this.element.style.top = top + 'px';
+    }
+}
+
+const food = new Food();
+food.change();
+console.log(food.X, food.Y)
--- webpack.config.js
+++ webpack.config.js
@@ -6,68 +6,89 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    //入口
-    entry: path.resolve(__dirname, "src/index.ts"),
-    //出口
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle1.js',
-        //打包函数不用箭头函数 兼容IE
-        // environment: {
-        //     arrowFunction: false
-        // }
-    },
-    mode: 'development', // 设置mode
-    //制定 webpack 使用的插件
-    module: {
-        //加载规则
-        rules: [
-            {
-                //test指定的是 规则生效的文件
-                test: /\.ts$/,
-                exclude: /node-modules/,
-                use: [
-                    {//配置babel
-                        loader: 'babel-loader',
-                        options: {
-                            //设置预定义的环境
-                            "presets": [
-                                [
-                                  "@babel/preset-env",
-                                //   {
-                                //     "targets": {
-                                //         "chrome": "58",
-                                //         // "ie": "11"
-                                //     },
-                                //     "corejs": "3",
-                                //     "useBuiltIns": "usage"
-                                //   }
-                                ]
-                              ]
-                        }
-                    }
-                    ,
-                    'ts-loader'
-                ]
+  //入口
+  entry: path.resolve(__dirname, "src/index.ts"),
+  //出口
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    //打包函数不用箭头函数 兼容IE
+    // environment: {
+    //     arrowFunction: false,
+    //     const: false,//不允许 const
+    // }
+  },
+  mode: "development", // 设置mode
+  devServer: {
+    host: "localhost",
+    port: 8080,
+  },
+  //制定 webpack 使用的插件
+  module: {
+    //加载规则
+    rules: [
+      {
+        //test指定的是 规则生效的文件
+        test: /\.ts$/,
+        exclude: /node-modules/,
+        use: [
+          {
+            //配置babel
+            loader: "babel-loader",
+            options: {
+              //设置预定义的环境
+              presets: [
+                [
+                  "@babel/preset-env",
+                    // {
+                    //   "targets": {
+                    //       "chrome": "58",
+                    //       "ie": "10"
+                    //   },
+                    //   "corejs": "3",
+                    //   "useBuiltIns": "usage"
+                    // }
+                ],
+              ],
             },
-            {
-                test: /\.less$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "less-loader"
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            title: 'ts-wabpack'
-        }),
-        new CleanWebpackPlugin()
+          },
+          "ts-loader",
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                      browsers: "last 2 versions",
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+          "less-loader",
+        ],
+      },
     ],
-    //来设置引用模块
-    resolve: {
-        extensions: ['.ts', '.js'],//ts 和 js都可以当做模块使用
-    }
-}
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: "ts-wabpack",
+      template: "src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  //来设置引用模块
+  resolve: {
+    extensions: [".ts", ".js"], //ts 和 js都可以当做模块使用
+  },
+};
